1aa57ecb7aa441f92990f72e1e80c13435d2ddfb
Francesco Pasa <francescopasa@gmail.com>
2014-10-05 22:14:37 +0200

    culo

--- e3/analisi/banda.py
+++ e3/analisi/banda.py
@@ -15,10 +15,12 @@ vin20 = ufloat(50, 0.5)
 db20 = 20.0 * unumpy.log10(vout20/vin20)
 m3db = db20[0].nominal_value - 3.0
 
+
 p3db = None
 for f1, db1, f2, db2 in zip(freq20[:-1], db20[:-1], freq20[1:], db20[1:]):
     if db1 > m3db and db2 < m3db:
         p3db = f1 + (f2 - f1) * (m3db - db1) / (db2 - db1)
+
 print(p3db)
 
 # 40 dB
@@ -39,25 +41,39 @@ for f1, db1, f2, db2 in zip(freq40[:-1], db40[:-1], freq40[1:], db40[1:]):
         p3db40 = f1 + (f2 - f1) * (m3db40 - db1) / (db2 - db1)
 print(p3db40)
 
-f1 = plt.figure()
-ax1 = f1.add_subplot(111)
+f1 = plt.figure(figsize=(10, 4))
+ax1 = f1.add_subplot(121)
 ax1.set_xscale('log')
 
-p2 = ax1.errorbar(x=freq20, y=unumpy.nominal_values(db20))
-ax1.plot([10, 1e6], [m3db, m3db])
-ax1.plot([p3db.nominal_value, p3db.nominal_value], [1, 24])
+p2 = ax1.errorbar(x=freq20, y=unumpy.nominal_values(db20), c="black", linewidth=2)
+ax1.plot([10, 1e6], [m3db, m3db], c="black")
+ax1.plot([108e3, 108e3], [0, 25], c="black")
 
+ax1.set_xlabel("Frequenza [Hz]")
+ax1.set_ylabel("Guadagno [dB]")
 ax1.set_xlim((10, 1e6))
+ax1.set_ylim((0, 25))
+ax1.grid(True)
+ax1.set_title("Guadagno 20 dB")
+ax1.text(y=16, x=20, s="-3 dB")
 
-f2 = plt.figure()
-ax2 = f2.add_subplot(111)
+#f2 = plt.figure()
+ax2 = f1.add_subplot(122)
 ax2.set_xscale('log')
 
-p2 = ax2.errorbar(x=freq40, y=unumpy.nominal_values(db40))
-ax2.plot([10, 1e6], [m3db40, m3db40])
-ax2.plot([p3db40.nominal_value, p3db40.nominal_value], [1, 40])
-ax2.plot([p3db40.nominal_value], [m3db40], "o")
+p2 = ax2.errorbar(x=freq40, y=unumpy.nominal_values(db40), c="black", linewidth=2)
+ax2.plot([10, 1e6], [m3db40, m3db40], c="black")
+ax2.plot([11e3, 11e3], [0, 50], c="black")
 
+ax2.set_xlabel("Frequenza [Hz]")
+ax2.set_ylabel("Guadagno [dB]")
 ax2.set_xlim((10, 1e6))
+ax2.set_ylim((0, 50))
+ax2.grid(True)
+ax2.set_title("Guadagno 40 dB")
+ax2.text(y=34, x=20, s="-3 dB")
+
+f1.subplots_adjust(right=0.95, left=0.07, bottom=0.15, top=0.9)
 
 plt.show()
+f1.savefig("../figure/freq_ris.pdf")
--- e3/analisi/freq.py
+++ e3/analisi/freq.py
@@ -11,7 +11,7 @@ vout_low = unumpy.uarray(data_low[:,2] / 2.0, data_low[:,4])
 
 G_low = 1001 * vout_low / va_low
 
-data_high = np.genfromtxt(open("../dati/guadagno_alta_freq.csv"), delimiter=",", skip_header=1)
+data_high = np.genfromtxt(open("../dati/guadagno_alta_freq_sum.csv"), delimiter=",", skip_header=1)
 
 freq_high = data_high[:,0] * 1000
 va_high = unumpy.uarray(data_high[:,1] / 2.0, data_high[:,3])
@@ -25,10 +25,22 @@ ax1 = f1.add_subplot(111)
 ax1.set_xscale('log')
 ax1.set_yscale('log')
 
-p1 = ax1.errorbar(x=freq_low, y=unumpy.nominal_values(G_low))
-p2 = ax1.errorbar(x=freq_high, y=unumpy.nominal_values(G_high))
+p1 = ax1.errorbar(x=freq_low, y=unumpy.nominal_values(G_low), fmt="o", c="black")
+p2 = ax1.errorbar(x=freq_high, y=unumpy.nominal_values(G_high), fmt="o", c="gray")
 
-ax1.set_xlim((1, 1e6))
+ax1.set_title("Guadagno di un opamp in funzione della frequenza")
+ax1.set_xlabel("Frequenza [Hz]")
+ax1.set_ylabel("Amplificazione")
+ax1.set_xlim((1, 3e6))
+ax1.set_ylim((1e-1, 1e6))
+ax1.grid(True)
 
+ax2 = ax1.twinx()
+ax2.set_yticks([0.0, 0.143, 0.286, 0.428, 0.571, 0.714, 0.857, 1.0])
+ax2.set_yticklabels(("-20", "0", "20", "40", "60", "80", "100", "120", "140"))
+ax2.set_ylabel("Amplificazione [dB]")
+
+ax2.legend((p1, p2), ("Bassa freq.", "Alta freq."))
 
 plt.show()
+f1.savefig("../figure/A_vs_freq.pdf")
